9b6749e50e6cd55dfa8e468f5930f95214a2c266
changsooooooooooo <lcs940621@gmail.com>
2021-10-19 15:15:51 +0900

    mapping revised

--- src/test/java/com/autotradeserver/repository/CoinListRepositoryTest.java
+++ src/test/java/com/autotradeserver/repository/CoinListRepositoryTest.java
@@ -8,11 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
